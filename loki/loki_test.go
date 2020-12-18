40d24946f3fad8d45e32eaf51e2639bdb62f5b35
Greg Hellings <greg@searchspring.com>
2020-12-17 17:08:57 -0800

    Fixing up test coverage

--- loki/loki_test.go
+++ loki/loki_test.go
@@ -1,11 +1,11 @@
 package loki
 
 import (
+	"io"
 	"io/ioutil"
-	"testing"
-
 	"net/http"
 	"net/http/httptest"
+	"testing"
 
 	"github.com/ghellings/cloudfront2loki/cflog"
 	"github.com/stretchr/testify/require"
@@ -26,16 +26,61 @@ func TestPushLogs(t *testing.T) {
 
 	defer ts.Close()
 	loki := New(ts.URL[7:])
-	logs := []*cflog.CFLog{
-		{Filename: "bob"},
-	}
+	logs := []*cflog.CFLog{mockCFLog("bogus-file1"), mockCFLog("bogus-file2")}
 	err := loki.PushLogs(logs, "{\"foo\": \"bar\"}")
 	require.NoError(t, err)
 	require.Contains(t, response, "foo")
 }
 
 func TestGetLatestLog(t *testing.T) {
-	loki := New("localhost:3100")
-	_, err := loki.GetLatestLog("{source=\"cloudfront\",job=\"cloudfront2loki\"}")
+	respstr := "{\"data\":{ \"result\": [ { \"values\": [[\"1\",\"Info: { \\\"Filename\\\": \\\"bogus-testfile\\\"}\"]]}]}}"
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, respstr)
+		_, err := ioutil.ReadAll(r.Body)
+		require.NoError(t, err)
+	}))
+	loki := New(ts.URL[7:])
+	filename, err := loki.GetLatestLog("{source=\"cloudfront\",job=\"cloudfront2loki\"}")
 	require.NoError(t, err)
+	require.Equal(t, filename, "bogus-testfile")
+}
+
+func mockCFLog(filename string) (log *cflog.CFLog) {
+	log = &cflog.CFLog{
+		Filename:                    filename,
+		Date:                        "-",
+		Time:                        "-",
+		X_edge_location:             "-",
+		Sc_bytes:                    "-",
+		C_ip:                        "-",
+		Cs_method:                   "-",
+		Cs_Host:                     "-",
+		Cs_uri_stem:                 "-",
+		Sc_status:                   "-",
+		Cs_Referer:                  "-",
+		Cs_User_Agent:               "-",
+		Cs_uri_query:                "-",
+		Cs_Cookie:                   "-",
+		X_edge_result_type:          "-",
+		X_edge_request_id:           "-",
+		X_host_header:               "-",
+		Cs_protocol:                 "-",
+		Cs_bytes:                    "-",
+		Time_taken:                  "-",
+		X_forwarded_for:             "-",
+		Ssl_protocol:                "-",
+		Ssl_cipher:                  "-",
+		X_edge_response_result_type: "-",
+		Cs_protocol_version:         "-",
+		Fle_status:                  "-",
+		Fle_encrypted_fields:        "-",
+		C_port:                      "-",
+		Time_to_first_byte:          "-",
+		X_edge_detailed_result_type: "-",
+		Sc_content_type:             "-",
+		Sc_content_len:              "-",
+		Sc_range_start:              "-",
+		Sc_range_end:                "-",
+	}
+	return
 }
--- s3/s3_test.go
+++ s3/s3_test.go
@@ -1,6 +1,9 @@
 package s3logs
 
 import (
+	"bytes"
+	"compress/gzip"
+	"fmt"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -10,44 +13,78 @@ import (
 )
 
 func TestNew(t *testing.T) {
+	require.Panics(
+		t,
+		func() { New("us-east-1", "bogus-bucket", "bogus-prefix", "NOTANUMBER") },
+		"Expected panic and didn't get it",
+	)
+
 	s3logs := New("us-east-1", "bogus-bucket", "bogus-prefix", "1")
 	require.NotNil(t, s3logs)
 }
 
 func TestGetListOfFiles(t *testing.T) {
 	s3client := &mockS3Client{
-		listobjectreturn: &s3.ListObjectsV2Output{},
+		listobjectreturn: &s3.ListObjectsV2Output{
+			Contents: []*s3.Object{
+				{Key: aws.String("bogus1")},
+				{Key: aws.String("bogus2")},
+			},
+		},
+	}
+	dlclient := &mockDLMgr{}
+	s3logs := &S3Logs{
+		s3client:      s3client,
+		dlmgr:         dlclient,
+		bucket:        "bogus-bucket",
+		prefix:        "cf-logs/PREFIX",
+		concurrency:   2,
+		dlconcurrency: 5,
+	}
+	files, nextfile, err := s3logs.getListofFiles("bogus")
+	require.NoError(t, err)
+	require.Equal(t, nextfile, "bogus2")
+	require.Equal(t, files, []*string{aws.String("bogus1"), aws.String("bogus2")})
+}
+
+func TestParseCFLogs(t *testing.T) {
+	s3client := &mockS3Client{
+		listobjectreturn: &s3.ListObjectsV2Output{
+			Contents: []*s3.Object{
+				{Key: aws.String("bogus1")},
+				{Key: aws.String("bogus2")},
+			},
+		},
 	}
 	dlclient := &mockDLMgr{}
 	s3logs := &S3Logs{
 		s3client:    s3client,
 		dlmgr:       dlclient,
-		bucket:      "b7i-sumologic",
-		prefix:      "cf-logs/E1OUPXPV64DT62",
+		bucket:      "bogus-bucket",
+		prefix:      "cf-logs/PREFIX",
 		concurrency: 2,
 	}
-	_, _, err := s3logs.getListofFiles("bogus")
-	require.NoError(t, err)
-}
-
-func TestParseCFLogs(t *testing.T) {
-	s3logs := New("us-east-1", "bogus-bucket", "bogus-prefix", "1")
-	buffer := []*wrbuffer{}
+	buffer := mockCompressedBuffer()
 	_, err := s3logs.parseCFLogs(buffer)
 	require.NoError(t, err)
 }
 
 func TestDownload(t *testing.T) {
 	s3client := &mockS3Client{
-		listobjectreturn: &s3.ListObjectsV2Output{},
+		listobjectreturn: &s3.ListObjectsV2Output{
+			Contents: []*s3.Object{
+				{Key: aws.String("bogus1")},
+				//	{ Key: aws.String("bogus2") },
+			},
+		},
 	}
 	dlclient := &mockDLMgr{}
 
 	s3logs := &S3Logs{
 		s3client:    s3client,
 		dlmgr:       dlclient,
-		bucket:      "b7i-sumologic",
-		prefix:      "cf-logs/E1OUPXPV64DT62",
+		bucket:      "bogus-bucket",
+		prefix:      "cf-logs/PREFIX",
 		concurrency: 2,
 	}
 	_, _, err := s3logs.Download("bogus-startafterfile")
@@ -64,6 +101,45 @@ func (m *mockS3Client) ListObjectsV2(list *s3.ListObjectsV2Input) (*s3.ListObjec
 
 type mockDLMgr struct{}
 
-func (m *mockDLMgr) DownloadWithIterator(aws.Context, s3manager.BatchDownloadIterator, ...func(*s3manager.Downloader)) error {
+func (m *mockDLMgr) DownloadWithIterator(ctx aws.Context, iter s3manager.BatchDownloadIterator, opts ...func(*s3manager.Downloader)) error {
+	wrbuffers := mockCompressedBuffer()
+	var pos int64 = 0
+	for _, wrbuffer := range wrbuffers {
+		w, err := iter.DownloadObject().Writer.WriteAt(wrbuffer.buffer.Bytes(), pos)
+		if err != nil {
+			panic(fmt.Sprintf("%v", err))
+		}
+		pos = int64(w)
+	}
 	return nil
+}
+
+func mockCompressedBuffer() (buffer []*wrbuffer) {
+	var buf bytes.Buffer
+	logtxt := "-"
+	for i := 1; i < 34; i++ {
+		logtxt = logtxt + "\t-"
+	}
+	logtxt = logtxt + "\n"
+	zw := gzip.NewWriter(&buf)
+	if _, err := zw.Write(
+		[]byte(logtxt)); err != nil {
+		panic(fmt.Sprintf("%v", err))
+	}
+	if err := zw.Close(); err != nil {
+		panic(fmt.Sprintf("%v", err))
+	}
+	awsbuf1 := aws.NewWriteAtBuffer(buf.Bytes())
+	awsbuf2 := aws.NewWriteAtBuffer(buf.Bytes())
+	buffer = []*wrbuffer{
+		{
+			filename: "bogus1",
+			buffer:   awsbuf1,
+		},
+		{
+			filename: "bogus2",
+			buffer:   awsbuf2,
+		},
+	}
+	return
 }
